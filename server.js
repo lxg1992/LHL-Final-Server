145eebfa1d106107ee49c25aed02819ee876006f
Alexey Garin <lxg1992@gmail.com>
2019-11-25 17:13:53 -0500

    Added CORS to allow for POSTs to work properly

--- server.js
+++ server.js
@@ -2,19 +2,21 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const knex = require('./knex/db.js');
 const bodyParser = require('body-parser');
-
-
+const cors = require('cors');
 
 const app = express()
+app.use(cors());
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//Test route
+app.get('/', (req, res) => {
+  res.send({ error: 'No resource at this route, try /users, /rooms, /guests, /questions' })
+})
 
 //USER ROUTES
-app.get('/', (req,res) => {
-  res.send({error: 'No resource at this route'})
-})
 //Get all users
-app.get('/users', async (req,res) => {
+app.get('/users', async (req, res) => {
   try {
     let result = await knex('users')
     res.json(result);
@@ -24,23 +26,23 @@ app.get('/users', async (req,res) => {
 })
 
 //Create a new user
-app.post('/users/register', async(req, res) => {
+app.post('/users/register', async (req, res) => {
   try {
     let form = req.body
-    let result = await knex('users').insert({first_name: form.first_name, last_name: form.last_name, email: form.email, password_hash: form.password}).returning('*')
+    let result = await knex('users').insert({ first_name: form.first_name, last_name: form.last_name, email: form.email, password_hash: form.password }).returning('*')
     res.json(result)
   } catch (error) {
     console.error(error)
   }
 })
 
-//Login
-app.post('/users/login', async(req,res) => {
+//Login a user
+app.post('/users/login', async (req, res) => {
   try {
     let form = req.body
-    let result = await knex('users').where({'email': form.email, 'password_hash': form.password}).returning('*')
-    if(!result.length){
-      res.json({error: 'Unable to verify user'})
+    let result = await knex('users').where({ 'email': form.email, 'password_hash': form.password }).returning('*')
+    if (!result.length) {
+      res.json({ error: 'Unable to verify user' })
     } else {
       res.json(result)
     }
@@ -49,64 +51,84 @@ app.post('/users/login', async(req,res) => {
   }
 })
 
-//Get specific user by id
-// app.get('/users/:id', async (req, res) => {
-//   try {
-//     let id = req.params.id;
-//     let result = await knex('users').where('id',id)
-//     res.json(result);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// })
 
-//get specific user by email
-app.get('/users/:email', async(req,res) => {
+//Get a user by email
+app.get('/users/:email', async (req, res) => {
   try {
     let email = req.params.email
-    let result = await knex('users').where('email',email)
-    if (result.length){
+    let result = await knex('users').where('email', email)
+    if (result.length) {
       res.json(result)
     } else {
-      res.json({error:"No such email"})
+      res.json({ error: "No such email" })
     }
   } catch (error) {
     console.error(error)
   }
 })
 
-
-app.get('/users/:id/rooms', async(req,res) => {
+//Get all rooms where userID is the host
+app.get('/users/:id/rooms/all', async (req, res) => {
   try {
+    let id = req.params.id
+    let result = await knex('rooms')
+      .where('host_id', id)
+    if (result.length) {
+      res.json(result)
+    } else {
+      res.json({ error: 'Either this user is not a host to any rooms, or no such user id exists' })
+    }
   } catch (error) {
-    
+    console.error(error)
   }
 })
 
 
 //ROOMS
 
+//get all rooms
 app.get('/rooms', async (req, res) => {
-  let result = await knex('rooms')
-  res.json(result)
+  try {
+    let result = await knex('rooms')
+    res.json(result)
+  } catch (error) {
+    console.error(error);
+  }
 })
 
-
-//get all rooms by host_id
 
 //get all questions by room hash
 app.get('/rooms/:hash/questions', async (req, res) => {
   try {
     let hash = req.params.hash
     let result = await knex
-      .select(['query','tags_selected'])
+      .select(['guests.guest_hash', 'questions.query', 'questions.tags_selected', 'questions.created_at'])
       .from('questions')
       .join('rooms', 'rooms.id', 'questions.room_id')
-      .where('rooms.room_hash',hash)
-    if(result.length){      
+      .join('guests', 'guests.room_id', 'rooms.id')
+      .where('rooms.room_hash', hash)
+    if (result.length) {
       res.json(result);
     } else {
-      res.json('Either')
+      res.json({ error: 'Either wrong hash or this room has no questions' })
+    }
+  } catch (error) {
+    console.error(error);
+  }
+})
+
+//Get tags_created for a room
+app.get('/rooms/:hash/tags', async (req, res) => {
+  try {
+    let hash = req.params.hash
+    let result = await knex
+      .select(['rooms.tags_created'])
+      .from('rooms')
+      .where('rooms.room_hash', hash)
+    if (result.length) {
+      res.json(result);
+    } else {
+      res.json({ error: 'Either wrong hash or this room has no questions' })
     }
   } catch (error) {
     console.error(error);
@@ -118,15 +140,15 @@ app.get('/rooms/:hash/guests', async (req, res) => {
   try {
     let hash = req.params.hash;
     let result = await knex
-    .select(['first_name', 'last_name', 'guests.guest_hash'])
-    .from('users')
-    .join('guests', 'guest_id', 'users.id')
-    .join('rooms', 'room_id', 'rooms.id' )
-    .where('rooms.room_hash', hash)
-    if(result.length){
+      .select(['first_name', 'last_name', 'guests.guest_hash'])
+      .from('users')
+      .join('guests', 'guest_id', 'users.id')
+      .join('rooms', 'room_id', 'rooms.id')
+      .where('rooms.room_hash', hash)
+    if (result.length) {
       res.json(result);
     } else {
-
+      res.json({ error: "Either this room does not exist or there are no guests here" })
     }
   } catch (error) {
     console.error(error);
@@ -134,50 +156,82 @@ app.get('/rooms/:hash/guests', async (req, res) => {
 })
 
 
-//Post a question into current rooms question DB
+//Post a question into current rooms question 
 app.post('/rooms/:hash/questions', async (req, res) => {
   try {
-    
-  } catch (error) {
-    
-  }
-})
+    let hash = req.params.hash
+    let query = req.body.message
+    let tags_selected = req.body.tags
+    let user_id = req.body.user_id
 
-//
+    console.log(`/rooms/${hash}/questions has been hit with:`)
+    console.log("hash, query, tags_selected, user_id")
+    console.log("===================================")
+    console.log(hash, query, tags_selected, user_id)
+    console.log("===================================")
+
+    let room_id_obj = await knex.select('rooms.id')
+      .from('rooms')
+      .where('rooms.room_hash', hash)
+
+    let room_id = room_id_obj[0].id
 
 
+    console.log(room_id)
+
+    //res.json(room_id)
 
 
+    let result = await knex('questions')
+      .insert({ user_id: user_id, room_id: room_id, query: query, tags_selected: JSON.stringify(tags_selected) })
+      .returning('*')
 
-//get specific question
-app.get('/questions/:id', async (req, res) => {
-  try {
-    let id = req.params.id;
-    let result = await knex('questions').where('id',id)
     res.json(result);
-    
   } catch (error) {
     console.error(error);
   }
 })
 
 
-app.post('/questions', async (req, res) => {
-  let msgs = req.body.messages;
-  let message = msgs.message;
-  let tags = msgs.tags;  
-  let result = await knex.insert({user_id: 1,query: message, tags_selected: JSON.stringify(tags)});
-  res.json({result});  
+//get specific question
+app.get('/questions/:id', async (req, res) => {
+  try {
+    let id = req.params.id;
+    let result = await knex('questions').where('id', id)
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+  }
 })
+
+
+
+
+
+
 //get all questions
 app.get('/questions', async (req, res) => {
-  try{
+  try {
     let result = await knex('questions')
     res.json(result)
   } catch (error) {
     console.error(error)
   }
 });
+
+
+
+
+app.get('/guests', async (req, res) => {
+  try {
+    let result = await knex('guests')
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+  }
+})
+
+
 
 
 app.listen(PORT, () => {
